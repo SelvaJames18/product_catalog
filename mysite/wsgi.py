d227085d67e40a3fc526223669c3f46d15fe5cc0
selva <selvajames418@gmail.com>
2025-05-04 19:06:08 +0530

    Fix Procfile for Django deployment

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'product_catlog.settings')  # Use your actual project settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'product_catlog.settings')
 
 application = get_wsgi_application()
